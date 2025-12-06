49f00c5063a53b850b34d1db446a8ad33170b738
Pedro Albuquerque <a7buquerque@gmail.com>
2025-12-06 19:35:35 -0300

    Instalando linter, autoformatter, lsp-server e debugger para python

--- lua/lazy-plugins.lua
+++ lua/lazy-plugins.lua
@@ -31,8 +31,6 @@ require('lazy').setup({
 
   require 'kickstart/plugins/lspconfig',
 
-  require 'kickstart/plugins/conform',
-
   require 'kickstart/plugins/blink-cmp',
 
   require 'kickstart/plugins/catppuccin',
@@ -42,6 +40,8 @@ require('lazy').setup({
   require 'kickstart/plugins/mini',
 
   require 'kickstart/plugins/treesitter',
+
+  require 'kickstart/plugins/none-ls',
 
   -- The following comments only work if you have downloaded the kickstart repo, not just copy pasted the
   -- init.lua. If you want these files, they are in the repository, so you can just download them and
